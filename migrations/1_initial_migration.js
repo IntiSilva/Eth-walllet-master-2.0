03484db4e27f23162ee71375cea4258fe375d977
Inti Silva <109829803+IntiSilva@users.noreply.github.com>
2023-06-11 02:44:47 -0300

    Delete migrations directory

--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-const USDTMock = artifacts.require("USDTMock");
-
-module.exports = async function(deployer) {
-  await deployer.deploy(Migrations);
-  await deployer.deploy(USDTMock);
-};
